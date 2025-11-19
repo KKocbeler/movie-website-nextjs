4153bbb8eaa3274bf172da6bfa2ba263dc65ccf0
Kemal Koçbeler <kkocbeler@gmail.com>
2025-11-19 21:03:55 +0300

    wrapped pages that use useSearchParams with suspence

--- app/(routes)/movies/page.tsx
+++ app/(routes)/movies/page.tsx
@@ -1,10 +1,11 @@
 import ListPage from '@/components/sections/listpage/ListPage'
+import { Suspense } from 'react'
 
 const page = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
         <ListPage title='Movies' type='movie'/>
-    </>
+    </Suspense>
   )
 }
 
--- app/(routes)/series/page.tsx
+++ app/(routes)/series/page.tsx
@@ -1,10 +1,11 @@
 import ListPage from '@/components/sections/listpage/ListPage';
+import { Suspense } from 'react';
 
 const page = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
         <ListPage title='Tv Series' type='tv'/>
-    </>
+    </Suspense>
   )
 }
 
